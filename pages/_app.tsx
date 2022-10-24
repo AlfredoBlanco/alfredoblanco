a4a57e5dbddc21b6bd27ce1b888df8fe067b36b6
Alfredo Blanco <alfre.blanco@icloud.com>
2022-10-24 19:18:10 -0300

    trying

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,11 @@ import type { AppProps } from 'next/app'
 import { EnglishProvider } from '../context/englishContext'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <EnglishProvider >
+      <Component {...pageProps} />
+    </EnglishProvider>
+  )
 }
 
 export default MyApp
